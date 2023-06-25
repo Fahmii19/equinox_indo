9b5f4dc5e2c5fed986847620eb38940bf4bf1dee
Fahmii19 <fhmialfi79@gmail.com>
2023-06-26 03:12:47 +0700

    first push

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    screens: {
+      xs: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
